e9afbb8504c7431efb44b1aa0d08f2f38f8999e6
Kenrick Buchanan <kenrick@specificperformance.com>
2013-08-09 13:14:46 -0700

    added boolean column with css labels

---
+++ GridView/Columns/BooleanColumn.php
@@ -0,0 +1,34 @@
+<?php namespace GridView\Columns;
+
+/**
+ * This column can be used to perform user specified calculations on any data
+ * that is found in $dataSource. It will return 0 if no calculation can be performed
+ * like if you forgot to specify a calculation callable
+ */
+class BooleanColumn extends Column {
+    
+	public $trueLabel = 'Yes';	
+    public $falseLabel = 'No';
+    public $trueValues = array('yes','true','1','on');
+    public $falseValues = array('no','false','0','off');
+
+	public function getValue($index)
+	{
+		$value = parent::getValue($index);
+        
+        // check and see if value is one of these:
+        // 1, true, yes
+        $labelHtml = '<span class="label %s">%s</span>';
+        $labelCss  = 'label-default';
+        
+        if( in_array(strtolower( (string) $value ), $this->trueValues, true) ) {
+            $labelCss = 'label-success';
+        }
+        
+        if( in_array(strtolower( (string) $value ), $this->falseValues, true) ) {
+            $labelCss = 'label-danger';
+        }
+        
+        return sprintf($labelHtml, $labelCss, $value);
+	}
+}
